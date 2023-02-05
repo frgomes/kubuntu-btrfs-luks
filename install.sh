4debb852b8e7b7dddb1be13e9379a96b9f1abb66
Richard Gomes <Richard Gomes>
2023-02-05 10:25:46 +0000

    WIP

--- install.sh
+++ install.sh
@@ -134,7 +134,7 @@ EOD
 }
 
 function configure_initramfs() {
-  grep -E '^#KEYFILE_PATTERN=$' /etc/cryptsetup-initramfs/conf-hook || sed -E 's|^#KEYFILE_PATTERN=|KEYFILE_PATTERN="/boot/*.key"|' /etc/cryptsetup-initramfs/conf-hook
+  grep -E '^#KEYFILE_PATTERN=$' /etc/cryptsetup-initramfs/conf-hook || sed 's|#KEYFILE_PATTERN=|KEYFILE_PATTERN="/boot/*.key"|' /etc/cryptsetup-initramfs/conf-hook
   # debugging
   cat /etc/cryptsetup-initramfs/conf-hook
 }
