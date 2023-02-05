04c1d8bd95ff18360225dcda1d18bb547d87d2b1
Richard Gomes <Richard Gomes>
2023-02-05 10:05:09 +0000

    WIP

--- install.sh
+++ install.sh
@@ -114,13 +114,21 @@ function grub_enable_cryptodisk() {
 function create_volume_unlock_keys() {
   local partition=/dev/nvme0n1p
   local passphrase="$(cat /dev/shm/luks_passphrase)"
-  dd bs=1 count=512 if=/dev/urandom of=/boot/volume-root.key
   dd bs=1 count=512 if=/dev/urandom of=/boot/volume-swap.key
+  dd bs=1 count=512 if=/dev/urandom of=/boot/volume-root.key
   echo "${passphrase}" | cryptsetup luksAddKey ${partition}2 /boot/volume-swap.key -
   echo "${passphrase}" | cryptsetup luksAddKey ${partition}4 /boot/volume-root.key -
   chmod 000 /boot/volume-swap.key
   chmod 000 /boot/volume-root.key
   chmod -R g-rwx,o-rwx /boot
+}
+
+function configure_crypttab() {
+  local partition=/dev/nvme0n1p
+  fgrep cryptroot /etc/cryptswap || cat <<EOD >> /etc/cryptab
+cryptswap ${partition}2 /boot/volume-swap.key luks,discard,key-slot=1
+cryptroot ${partition}2 /boot/volume-root.key luks,discard,key-slot=2
+EOD
 }
 
 
@@ -133,4 +141,7 @@ function automated_install() {
   create_fstab
   install_grub
   grub_enable_cryptodisk
+  configure_crypttab
+
+
 }
