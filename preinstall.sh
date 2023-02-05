49298fe3edc88c4ffc8267fd7b189f6be1ed7516
Richard Gomes <Richard Gomes>
2023-02-05 05:16:58 +0000

    WIP

--- preinstall.sh
+++ preinstall.sh
@@ -99,71 +99,11 @@ function setup_chroot() {
   # setup networking
   cp /etc/resolv.conf /mnt/etc
   # copy scripts
-  local dir=$(dirname $(readlink -f "${BASH_SOURCE[0]}")
-  mkdir -p /mnt/tmp/chroot_install
-  cp -pv ${dir}/*.sh /mnt/tmp/chroot_install
+  local dir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+  mkdir -p /mnt/tmp/chroot
+  cp -pv ${dir}/*.sh /mnt/tmp/chroot
+  chroot /mnt /bin/bash
 }
-
-function install_locales() {
-  apt update
-  apt install -y locales
-  apt-reconfigure locales
-}
-
-function install_btrfs_progs() {
-  apt install -y btrfs-progs crypsetup
-}
-
-function install_kernel() {
-  ##FIXME: should detect hardware architecture
-  local firmware=$(apt search firmware | grep -E "^firmware-" | cut -d/ -f1 | fgrep -v microbit)
-  apt install -y linux-image-amd64 intel-microcode amd64-microcode 
-}
-
-function setup_root() {
-  local password=password
-  local confirm=wrong
-  while [ "${password}" != "${confirm}" ] ;do
-    echo -n "Enter password for root: "
-    read -s password
-    echo -n "Confirm password for root: "
-    read -s confirm
-  done
-  echo "${password}" | passwd root --stdin
-}
-
-function setup_user() {
-  local fullname="Debian"
-  echo -n "Enter full name of first user: "
-  read fullname
-
-  local username=$(echo "${fullname}" | cut -d' ' -f1 | tr '[:upper:]' '[:lower:]')
-  echo -n "Enter username of first user: "
-  read username
-
-  local password=password
-  local confirm=wrong
-  while [ "${password}" != "${confirm}" ] ;do
-    echo -n "Enter password for ${username}: "
-    read -s password
-    echo -n "Confirm password for ${username}: "
-    read -s confirm
-  done
-  echo "${password}" | passwd ${username} --stdin
-}
-
-function create_fstab() {
-  local device=/dev/nvme0n1
-  local uuid_efi=$(blkid | fgrep ${device}p1 | cut -d' ' -f2 | sed 's/"//g' | tr '[:lower:]' '[:upper:]')
-  local uuid_swap=$(blkid | fgrep ${device}p2 | cut -d' ' -f2 | sed 's/"//g' | tr '[:lower:]' '[:upper:]')
-  local uuid_boot=$(blkid | fgrep ${device}p3 | cut -d' ' -f2 | sed 's/"//g' | tr '[:lower:]' '[:upper:]')
-  local uuid_root=$(blkid | fgrep ${device}p4 | cut -d' ' -f2 | sed 's/"//g' | tr '[:lower:]' '[:upper:]')
-  cat /proc/mounts | grep -E "^/dev/mapper/|^tmpfs|^${device}" | \
-    sed -E "s|^/dev/mapper/cryptroot|${uuid_root}|" | \
-    sed -E "s|^tmpfs|${uuid_root}|" | \
-    sed -E "s|${device}p1|${uuid_efi}|"
-}
-
 
 function automated_install() {
   make_partitions
@@ -173,6 +113,6 @@ function automated_install() {
   mount_volumes
   install_debian
   update_sources
-  setup_chroot
+  create_chroot
   chroot /mnt /tmp/chroot/install.sh
 }
