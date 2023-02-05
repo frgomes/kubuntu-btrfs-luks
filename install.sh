d1537b8fbcbb12e065d7abca463af25f02d5e521
Richard Gomes <Richard Gomes>
2023-02-05 10:11:03 +0000

    WIP

--- install.sh
+++ install.sh
@@ -125,10 +125,12 @@ function create_volume_unlock_keys() {
 
 function configure_crypttab() {
   local partition=/dev/nvme0n1p
-  fgrep "${partition}" /etc/crypttab || cat <<EOD >> /etc/cryptab
+  fgrep "${partition}" /etc/crypttab || cat <<EOD >> /etc/crypttab
 cryptswap ${partition}2 /boot/volume-swap.key luks,discard,key-slot=1
 cryptroot ${partition}4 /boot/volume-root.key luks,discard,key-slot=2
 EOD
+  # debugging
+  cat /etc/crypttab
 }
 
 
