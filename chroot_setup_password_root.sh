ce76bc00f8fbbe7a8a59475f2b003b64c4ab115e
Richard Gomes <Richard Gomes>
2023-02-11 22:08:51 +0000

    WIP

--- chroot_setup_password_root.sh
+++ chroot_setup_password_root.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -eux
 
 function chroot_setup_password_root() {
-  dd if=/dev/urandom count=1 bs=32 | base64 | passwd --quiet root > /dev/null
-  cat /dev/shm/root_password | passwd --quiet root
+  local password="$(cat /dev/shm/root_password)"
+  echo "${password}\n${password}" | passwd --quiet root
 }
 
 chroot_setup_password_root
--- chroot_setup_password_user.sh
+++ chroot_setup_password_user.sh
@@ -4,8 +4,8 @@ function chroot_setup_password_user() {
   local fullname=$(cat /dev/shm/user_fullname)
   local username=$(cat /dev/shm/user_username)
   useradd -m "${username}" -c "${fullname}"
-  dd if=/dev/urandom count=1 bs=32 | base64 | passwd --quiet "${username}" > /dev/null
-  cat /dev/shm/user_password | passwd --quiet "${username}"
+  local password="$(cat /dev/shm/user_password)"
+  echo "${password}\n${password}" | passwd --quiet "${username}"
 }
 
 chroot_setup_password_user
