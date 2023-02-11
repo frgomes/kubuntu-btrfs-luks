809bca413b8618e7e9aabbb279aa279e1d0cd5dc
Richard Gomes <Richard Gomes>
2023-02-11 23:36:24 +0000

    WIP

--- chroot_install_locales.sh
+++ chroot_install_locales.sh
@@ -5,12 +5,22 @@ function chroot_install_locales() {
   local keyboard="$(cat /dev/shm/keyboard)"
   local language="$(cat /dev/shm/language)"
   local timezone="$(cat /dev/shm/timezone)"
+
+  cat <<EOD > /etc/default/keyboard
+# KEYBOARD CONFIGURATION FILE
+# Consult the keyboard(5) and xkeyboard-config(7) manual page.
+
+XKBMODEL="pc105"
+XKBLAYOUT="${keyboard}"
+XKBVARIANT=""
+XKBOPTIONS="grp:alt_shift_toggle"
+BACKSPACE="guess"
+EOD
+
   apt update
   apt install -y locales
   locale-gen "${language}.UTF-8"
-  localectl set-keymap --no-convert "${keyboard}"
-  localectl set-locale LC_ALL="${language}.UTF-8"
-  timedatectl set-timezone ${timezone}
+  update-locale LANG="${language}.UTF-8"
 }
 
 chroot_install_locales
