91678a43b2fe1660ce6ebc8adf8ba790e88b3c28
Richard Gomes <Richard Gomes>
2023-02-11 14:56:33 +0000

    WIP

--- pre-install.sh
+++ pre-install.sh
@@ -142,6 +142,7 @@ function setup_chroot() {
 
 function deploy_chroot_scripts() {
   [[ -d /mnt/tmp/chroot ]] || rm -r -f /mnt/tmp/chroot
+  [[ -d /mnt/tmp/kubuntu-btrfs-luks ]] || rm -r -f /mnt/tmp/kubuntu-btrfs-luks
   local dir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
   mkdir -p /mnt/tmp
   cp -rp ${dir} /mnt/tmp
