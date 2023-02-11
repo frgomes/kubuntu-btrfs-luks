ba82e1ce5c19db828ee098f94de3fd128b0a2212
Richard Gomes <Richard Gomes>
2023-02-11 23:01:57 +0000

    WIP

--- chroot_install_locales.sh
+++ chroot_install_locales.sh
@@ -5,23 +5,11 @@ function chroot_install_locales() {
   local keyboard="$(cat /dev/shm/keyboard)"
   local language="$(cat /dev/shm/language)"
   local timezone="$(cat /dev/shm/timezone)"
-
-  loadkeys "${keyboard}"
-  sudo dpkg-reconfigure --frontend noninteractive console-setup
-
-  # define default locale configuration
   apt update
   apt install -y locales
-
-  ##XXX locale-gen "${lang}.UTF-8"
-  ##XXX dpkg-reconfigure --frontend noninteractive locales
-  ##XXX update-locale "LANG=${lang}.UTF-8"
-
-  echo "locales locales/default_environment_locale select      ${language}.UTF-8"       | debconf-set-selections
-  echo "locales locales/locales_to_be_generated    multiselect ${language}.UTF-8 UTF-8" | debconf-set-selections
-  rm "/etc/locale.gen"
-  dpkg-reconfigurel --frontend noninteractive locales
-
+  locale-gen "${language}.UTF-8"
+  localectl set-keymap --no-convert "${keyboard}"
+  localectl set-locale LC_ALL="${language}.UTF-8"
   timedatectl set-timezone ${timezone}
 }
 
