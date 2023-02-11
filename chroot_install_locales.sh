d7a1671dad41031adf6248d0c4adf48136bc07e8
Richard Gomes <Richard Gomes>
2023-02-11 23:53:32 +0000

    WIP

--- chroot_install_locales.sh
+++ chroot_install_locales.sh
@@ -20,7 +20,7 @@ EOD
   apt update
   apt install -y locales
   locale-gen "${language}.UTF-8"
-  update-locale "LANG=${language}.UTF-8"
+  update-locale "LC_ALL=${language}.UTF-8"
   locale-gen --purge "${language}.UTF-8"
 }
 
