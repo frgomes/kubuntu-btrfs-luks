e5c88624b9632f81893316cb8b786f7aa6c66838
Richard Gomes <Richard Gomes>
2023-02-11 16:33:41 +0000

    WIP

--- pre-install.sh
+++ pre-install.sh
@@ -304,25 +304,25 @@ function automated_install() {
   define_user_password
   echo -n "PRESS ENTER"; read -s dummy
 
-  make_partitions "${passphrase}"
-  echo -n "PRESS ENTER"; read -s dummy
-  define_luks_passphrase
-  echo -n "PRESS ENTER"; read -s dummy
-  make_luks
-  echo -n "PRESS ENTER"; read -s dummy
-  make_filesystems
-  echo -n "PRESS ENTER"; read -s dummy
-  make_volumes
-  echo -n "PRESS ENTER"; read -s dummy
-  mount_volumes
-  echo -n "PRESS ENTER"; read -s dummy
-  install_debian
-  echo -n "PRESS ENTER"; read -s dummy
-  update_sources
-  echo -n "PRESS ENTER"; read -s dummy
-
-  setup_chroot
-  echo -n "PRESS ENTER"; read -s dummy
+  # make_partitions "${passphrase}"
+  # echo -n "PRESS ENTER"; read -s dummy
+  # define_luks_passphrase
+  # echo -n "PRESS ENTER"; read -s dummy
+  # make_luks
+  # echo -n "PRESS ENTER"; read -s dummy
+  # make_filesystems
+  # echo -n "PRESS ENTER"; read -s dummy
+  # make_volumes
+  # echo -n "PRESS ENTER"; read -s dummy
+  # mount_volumes
+  # echo -n "PRESS ENTER"; read -s dummy
+  # install_debian
+  # echo -n "PRESS ENTER"; read -s dummy
+  # update_sources
+  # echo -n "PRESS ENTER"; read -s dummy
+  #
+  # setup_chroot
+  # echo -n "PRESS ENTER"; read -s dummy
 
   deploy_chroot_scripts
   echo -n "PRESS ENTER"; read -s dummy
