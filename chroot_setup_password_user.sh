99dff8e23c04742d31ecf1ade5021f7230b02a2a
Richard Gomes <Richard Gomes>
2023-02-11 23:03:28 +0000

    WIP

--- chroot_setup_password_user.sh
+++ chroot_setup_password_user.sh
@@ -3,9 +3,8 @@
 function chroot_setup_password_user() {
   local fullname=$(cat /dev/shm/user_fullname)
   local username=$(cat /dev/shm/user_username)
-  useradd -m "${username}" -c "${fullname}"
-  local username="$(cat /dev/shm/user_username)"
   local password="$(cat /dev/shm/user_password)"
+  [[ -d /home/${username} ]] || useradd -m "${username}" -c "${fullname}"
   echo "${username}:${password}" | chpasswd
 }
 
