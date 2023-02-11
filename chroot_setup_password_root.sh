732fe4613c2327fb5d62baebf51494dc5c02dae6
Richard Gomes <Richard Gomes>
2023-02-11 22:20:09 +0000

    WIP

--- chroot_setup_password_root.sh
+++ chroot_setup_password_root.sh
@@ -2,7 +2,7 @@
 
 function chroot_setup_password_root() {
   local password="$(cat /dev/shm/root_password)"
-  echo "${password}\n${password}" | passwd --quiet root
+  echo "root:${password}" | chpasswd
 }
 
 chroot_setup_password_root
--- chroot_setup_password_user.sh
+++ chroot_setup_password_user.sh
@@ -4,8 +4,9 @@ function chroot_setup_password_user() {
   local fullname=$(cat /dev/shm/user_fullname)
   local username=$(cat /dev/shm/user_username)
   useradd -m "${username}" -c "${fullname}"
+  local username="$(cat /dev/shm/user_username)"
   local password="$(cat /dev/shm/user_password)"
-  echo "${password}\n${password}" | passwd --quiet "${username}"
+  echo "${username}:${password}" | chpasswd
 }
 
 chroot_setup_password_user
