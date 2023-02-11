03210fa54f3489f5f4053d79ad2bb5e5c7eded1b
Richard Gomes <Richard Gomes>
2023-02-11 20:09:54 +0000

    WIP

--- chroot_configure_initramfs.sh
+++ chroot_configure_initramfs.sh
@@ -2,7 +2,7 @@
 
 function chroot_configure_initramfs() {
   echo "[ configure_initramfs ]"
-  fgrep '#KEYFILE_PATTERN=' /etc/cryptsetup-initramfs/conf-hook > /dev/null || sed 's|#KEYFILE_PATTERN=|KEYFILE_PATTERN="/boot/*.key"|' -i /etc/cryptsetup-initramfs/conf-hook
+  sed -E 's|^#KEYFILE_PATTERN=[ \t]*$|KEYFILE_PATTERN="/boot/*.key"|' -i /etc/cryptsetup-initramfs/conf-hook
   cat /etc/cryptsetup-initramfs/conf-hook
 }
 
