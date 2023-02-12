5d06125a6b3973c7f5c8659013007eb79f63fbf4
Richard Gomes <Richard Gomes>
2023-02-12 22:46:11 +0000

    WIP

--- shortcut-upate-kernel.sh
+++ shortcut-upate-kernel.sh
@@ -3,6 +3,19 @@
 source ./functions_bootstrap.sh
 source ./functions_chroot.sh
 
+function define_filesystems() {
+  echo "[ make_filesystems ]"
+  local device=$(cat /dev/shm/device)
+  local partition=${device}p
+  local passphrase=$(cat /dev/shm/luks_passphrase)
+  echo -n "${passphrase}" | cryptsetup luksOpen   -d -              ${partition}2 cryptswap
+  echo -n "${passphrase}" | cryptsetup luksOpen   -d -              ${partition}4 cryptroot
+  lsblk
+}
+
+define_filesystems
+read -p "Press ENTER"
+
 mount_volumes
 read -p "Press ENTER"
 
