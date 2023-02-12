5eb78b6b6009d6f40de84bb63a403017a0aff88c
Richard Gomes <Richard Gomes>
2023-02-12 08:53:07 +0000

    WIP

--- chroot_install_btrfs_progs.sh
+++ chroot_install_btrfs_progs.sh
@@ -4,9 +4,7 @@ function chroot_install_btrfs_progs() {
   echo "[ install_btrfs_progs ]"
   apt update
   ##FIXME: handle retries
-  apt install -y btrfs-progs cryptsetup snapper
-  apt install -y btrfs-progs cryptsetup snapper
-  apt install -y btrfs-progs cryptsetup snapper
+  DEBIAN_FRONTEND=noninteractive apt install -y btrfs-progs cryptsetup snapper
 }
 
 chroot_install_btrfs_progs
