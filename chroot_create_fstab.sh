41b255e42a6fd4286f005b35f0e28b7aa9333190
Richard Gomes <Richard Gomes>
2023-02-12 15:58:11 +0000

    WIP

--- chroot_create_fstab.sh
+++ chroot_create_fstab.sh
@@ -10,12 +10,12 @@ function chroot_create_fstab() {
   local uuid_root=$(blkid | fgrep ${partition}4 | cut -d' ' -f2 | sed 's/"//g' | tr '[:lower:]' '[:upper:]')
   cat /proc/mounts | grep -v -E '^tmpfs' | grep -E "^/dev/mapper/|^${partition}" | \
     sed -E "s|^/dev/mapper/cryptroot|${uuid_root}|" | \
-    sed -E "s|^tmpfs|${uuid_root}|" | \
+    sed -E "s|${partition}1|${uuid_efi}|" | \
+    sed -E "s|^${partition}3|${uuid_boot}|" | \
     sed -E "s|/ btrfs|/           btrfs|" | \
     sed -E "s|/home btrfs|/home       btrfs|" | \
     sed -E "s|/@ 0 0|/           0 0|" | \
-    sed -E "s|/@home 0 0|/home       0 0|" | \
-    sed -E "s|${partition}1|${uuid_efi}|" > /etc/fstab
+    sed -E "s|/@home 0 0|/home       0 0|" | > /etc/fstab
   # debugging
   cat /etc/fstab
 }
